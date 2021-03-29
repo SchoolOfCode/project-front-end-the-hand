f30d1caefbd3f7e2105b72aa251d84907e0af533
jamesgreygoose <james@schoolofcode.co.uk>
2021-03-29 12:21:05 +0100

    feedback for front end

--- src/Component/App/Index.js
+++ src/Component/App/Index.js
@@ -1,53 +1,49 @@
-import './App.css';
-import Header from '../Header/';
-import Button from '../Button';
-import RestInfo from '../RestInfo';
-import BookingPage from '../BookingPage';
-import ReservationPage from '../ReservationPage';
+import "./App.css";
+import Header from "../Header/";
+import Button from "../Button";
+import RestInfo from "../RestInfo";
+import BookingPage from "../BookingPage";
+import ReservationPage from "../ReservationPage";
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
 function App() {
   return (
     <Router>
-    <div className="App">
-      <nav class="nav">
-              <ul>
-                <li>
-                  <Link to="/">Landing Page</Link>
-                </li>
-                <li>
-                  <Link to="/recs">Recommendations</Link>
-                </li>
-                <li>
-                  <Link to="/bookings">Booking Page</Link>
-                </li>
-                <li>
-                  <Link to="/reservations">Reservations</Link>
-                </li>
-              </ul>
-            </nav>
+      <div className="App">
+        <nav class="nav">
+          <ul>
+            <li>
+              <Link to="/">Landing Page</Link>
+            </li>
+            <li>
+              <Link to="/recs">Recommendations</Link>
+            </li>
+            <li>
+              <Link to="/bookings">Booking Page</Link>
+            </li>
+            <li>
+              <Link to="/reservations">Reservations</Link>
+            </li>
+          </ul>
+        </nav>
 
-<Switch>
-              <Route path="/bookings">
-                <BookingPage />
-              </Route>
-              <Route path="/recs">
-                <RestInfo/>
-              </Route>
-              <Route path="/reservations">
-                <ReservationPage />
-              </Route>
-              <Route path="/">
-                <Header />
-              </Route>
-            </Switch>
-
-    
-        
-     
-        </div>
-        </Router>
+        <Switch>
+          <Route path="/bookings">
+            <BookingPage />
+          </Route>
+          <Route path="/recs">
+            <RestInfo />
+          </Route>
+          <Route path="/reservations">
+            <ReservationPage />
+          </Route>
+          <Route path="/">
+            <Header />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
-  }
+}
 
 export default App;
--- src/Component/Header/index.js
+++ src/Component/Header/index.js
@@ -1,17 +1,15 @@
-import React from 'react';
-import css from './header.module.css';
-
+import React from "react";
+import css from "./header.module.css";
 
 function Header() {
-    return (
-        <div className={css.header}>
-            <h1 className={css.title}>BOOKAVO <span className={css.tag}>Experience your city through food.</span></h1>
-       
-        </div>
-    )
+  return (
+    <div className={css.header}>
+      <h1 className={css.title}>
+        BOOKAVO{" "}
+        <span className={css.tag}>Experience your city through food.</span>
+      </h1>
+    </div>
+  );
 }
 
-
-
 export default Header;
-
